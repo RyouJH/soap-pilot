eb0446f1612367b3916a62d8fd7121c6e5ffcaed
Ryou Jae Hyung <rjh926@gmail.com>
2025-01-13 16:24:04 +0900

    wsdl converter test code fix

--- src/test/java/soap/pilot/SwaggerConverterTest.java
+++ src/test/java/soap/pilot/SwaggerConverterTest.java
@@ -2,14 +2,17 @@ package soap.pilot;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.parser.OpenAPIV3Parser;
+import org.junit.Assert;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import soap.pilot.parser.OpenApiParser;
 import soap.pilot.parser.WSDLBuilder;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
-import java.io.FileWriter;
+import java.io.FileInputStream;
+import java.io.StringWriter;
 import java.nio.charset.StandardCharsets;
 
 public class SwaggerConverterTest {
@@ -22,8 +25,23 @@ public class SwaggerConverterTest {
         OpenAPI openAPI = new OpenAPIV3Parser().readContents(yaml).getOpenAPI();
         WSDLBuilder wsdlBuilder = new WSDLBuilder();
         OpenApiParser.parse(openAPI, wsdlBuilder);
-        try (FileWriter writer = new FileWriter("target/result.wsdl")) {
+        try (StringWriter writer = new StringWriter()) {
             wsdlBuilder.write(writer);
+            Assert.assertEquals(readFromFile(new File(resourceDir,"wsdl_expect.wsdl")),writer.toString());
+            System.out.println(writer);
+        }
+
+    }
+
+    private static String readFromFile(File f) throws Exception {
+        try (FileInputStream fis = new FileInputStream(f);
+             ByteArrayOutputStream out = new ByteArrayOutputStream()) {
+            byte[] buf = new byte[2048];
+            int nRead = -1;
+            while ((nRead = fis.read(buf)) != -1) {
+                out.write(buf, 0, nRead);
+            }
+            return new String(out.toByteArray(), StandardCharsets.UTF_8);
         }
     }
 }
