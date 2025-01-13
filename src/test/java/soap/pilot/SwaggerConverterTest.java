03c9f83eae743570ed2f1db090318a17d060fc80
Ryou Jae Hyung <rjh926@gmail.com>
2025-01-13 16:26:07 +0900

    wsdl converter test code fix

--- src/test/java/soap/pilot/SwaggerConverterTest.java
+++ src/test/java/soap/pilot/SwaggerConverterTest.java
@@ -9,9 +9,7 @@ import org.slf4j.LoggerFactory;
 import soap.pilot.parser.OpenApiParser;
 import soap.pilot.parser.WSDLBuilder;
 
-import java.io.ByteArrayOutputStream;
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.StringWriter;
 import java.nio.charset.StandardCharsets;
 
@@ -25,23 +23,11 @@ public class SwaggerConverterTest {
         OpenAPI openAPI = new OpenAPIV3Parser().readContents(yaml).getOpenAPI();
         WSDLBuilder wsdlBuilder = new WSDLBuilder();
         OpenApiParser.parse(openAPI, wsdlBuilder);
+        String expected = TestUtils.readFile(new File(resourceDir, "wsdl_expect.wsdl"), StandardCharsets.UTF_8);
         try (StringWriter writer = new StringWriter()) {
             wsdlBuilder.write(writer);
-            Assert.assertEquals(readFromFile(new File(resourceDir,"wsdl_expect.wsdl")),writer.toString());
+            Assert.assertEquals(expected, writer.toString());
             System.out.println(writer);
-        }
-
-    }
-
-    private static String readFromFile(File f) throws Exception {
-        try (FileInputStream fis = new FileInputStream(f);
-             ByteArrayOutputStream out = new ByteArrayOutputStream()) {
-            byte[] buf = new byte[2048];
-            int nRead = -1;
-            while ((nRead = fis.read(buf)) != -1) {
-                out.write(buf, 0, nRead);
-            }
-            return new String(out.toByteArray(), StandardCharsets.UTF_8);
         }
     }
 }
